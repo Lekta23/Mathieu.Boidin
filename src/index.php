0dd3526700b282fdee3c170a74011045e4a43212
Lekta23 <48383181+Lekta23@users.noreply.github.com>
2022-01-10 16:10:01 +0100

    Formulaire

--- src/index.php
+++ src/index.php
@@ -8,3 +8,33 @@ $nom = "Boidin";
 echo $prenom . " " . $nom;
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Identité</title>
+</head>
+<body>
+
+<form action="index.php" method="post">
+ <p>Votre nom : <input type="text" name="nom" /></p>
+ <p>Votre âge : <input type="text" name="age" /></p>
+ <p><input type="submit" value="OK" name="OK"></p>
+</form>
+
+<?php if(isset($_POST['OK'])){ ?>
+    <p>Bonjour, <?php echo htmlspecialchars($_POST['nom']); ?>.</p>
+<p>Tu as <?php echo (int)$_POST['age']; ?> ans.</p>
+
+<?php } ?>
+
+
+
+    
+</body>
+</html>
+
+
+
