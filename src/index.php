f2a2ce15bb3be0408f9fe0cee5e2c154ab8d6e10
Lekta23 <48383181+Lekta23@users.noreply.github.com>
2022-01-10 15:50:08 +0100

    Le changement de date

--- src/index.php
+++ src/index.php
@@ -1,9 +1,10 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 2 déc. 2021 à 16h31 </p>';
-phpinfo();
 
-echo "Mathieu Boidin";
+$prenom = "Mathieu";
+$nom = "Boidin";
+
+echo $prenom . " " . $nom;
 ?>
 
